f6a43d39754424738218e010d4c9af75722f5181
Viral-devani <114668248+Viral-devani@users.noreply.github.com>
2022-12-21 16:19:07 +0000

    Update LogoutPage.java

--- src/main/java/com/tutorialninja/pages/LogoutPage.java
+++ src/main/java/com/tutorialninja/pages/LogoutPage.java
@@ -15,7 +15,7 @@ public class LogoutPage extends Utility {
     public LogoutPage(){
         PageFactory.initElements(driver, this);
     }
-    //3.16 Verify the text “Account Logout”
+    
     @CacheLookup
     @FindBy(xpath = "//h1[text()='Account Logout']")
     WebElement getLogoutText;
@@ -23,11 +23,11 @@ public class LogoutPage extends Utility {
     @FindBy(xpath = "//a[contains(text(),'Continue')]")
     WebElement clickContinueButton;
     public String getAccountLogoutText(){
-        //Verify text "Account Logout"
+        
         log.info("Verify text \"Account Logout\" " + getLogoutText.toString());
         return getTextFromElement(getLogoutText);
     }
-    //3.17 Click on Continue button
+    
     public void clickOnContinueButton(){
         clickOnElement(clickContinueButton);
         log.info("Clicking on Continue Button " + clickContinueButton.toString());
